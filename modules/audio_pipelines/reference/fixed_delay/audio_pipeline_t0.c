f86e575f1670dda83d0410b1ded63ddac91e3c2d
Kevin Ahrendt <kahrendt@gmail.com>
2024-09-15 11:46:08 -0400

    copy IC and NS processed audio into ref

--- modules/audio_pipelines/reference/fixed_delay/audio_pipeline_t0.c
+++ modules/audio_pipelines/reference/fixed_delay/audio_pipeline_t0.c
@@ -97,7 +97,8 @@ static void stage_vnr_and_ic(frame_data_t *frame_data)
     ic_adapt(&ic_stage_state.state, vnr_pred_stage_state.vnr_pred_state.input_vnr_pred);
 
     /* Intentionally ignoring comms ch from here on out */
-    memcpy(frame_data->samples, ic_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));
+    memcpy(frame_data->samples[0], ic_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));
+    memcpy(frame_data->aec_reference_audio_samples[0], ic_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));   // Store the interference cancelled audio in the first reference channel
 #endif
 }
 
@@ -111,7 +112,8 @@ static void stage_ns(frame_data_t *frame_data)
                 &ns_stage_state.state,
                 ns_output,
                 frame_data->samples[0]);
-    memcpy(frame_data->samples, ns_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));
+    memcpy(frame_data->samples[0], ns_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));
+    memcpy(frame_data->aec_reference_audio_samples[1], ns_output, appconfAUDIO_PIPELINE_FRAME_ADVANCE * sizeof(int32_t));   // Store NS audio in the second reference channel
 #endif
 }
 
